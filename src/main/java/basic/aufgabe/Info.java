75ea773f1e8deb086e591a0cad676d5c157cf678
Dmitry <dmitry.pakulov@gmail.com>
2022-01-10 14:04:53 +0100

    Aufgabe 2: WuerfelStatistik Dmitry

--- src/main/java/basic/aufgabe/Info.java
+++ src/main/java/basic/aufgabe/Info.java
@@ -1,9 +1,7 @@
 package basic.aufgabe;
 
 public class Info {
-
     public static void main(String[] args){
         System.out.println("Dmitry Pakulov");
     }
-
 }
--- src/main/java/basic/aufgabe2/WuerfelStatistik.java
+++ src/main/java/basic/aufgabe2/WuerfelStatistik.java
@@ -24,10 +24,41 @@ package basic.aufgabe2;
 
 public class WuerfelStatistik {
 	public static void main(String[] args) {
+		int max = 6;
+		int min = 1;
+		int durchlaeufe = 10000;
+		int [] statistik = {0, 0, 0, 0, 0, 0};
+		double startTime = System.currentTimeMillis();
 
+		for(int i = 0; i<durchlaeufe; i++)
+		{
+			int würfelzahl = (int)(Math.random() * max + min);
 
-		double zufall = Math.random();
+			switch(würfelzahl){
+				case 1:
+					statistik[0] = ++statistik[0];
+					break;
+				case 2:
+					statistik[1] = ++statistik[1];
+					break;
+				case 3:
+					statistik[2] = ++statistik[2];
+					break;
+				case 4:
+					statistik[3] = ++statistik[3];
+					break;
+				case 5:
+					statistik[4] = ++statistik[4];
+					break;
+				case 6:
+					statistik[5] = ++statistik[5];
+					break;
+			}
+		}
+		for(int a : statistik){
+			System.out.println(a);
+		}
 
-		// hier gehts los....
+		System.out.println(System.currentTimeMillis() - startTime + " Millisekunden");
 	}
 }
