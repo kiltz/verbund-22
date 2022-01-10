395af0be00867c62102ecd4fc4be8d7ba95efd11
Tobias Engelmann <engelmanntobi@web.de>
2022-01-10 11:46:22 +0100

    netter Text. Tobi

--- src/main/java/basic/aufgabe/Info.java
+++ src/main/java/basic/aufgabe/Info.java
@@ -1,8 +1,7 @@
 package basic;
 
-public class HalloWelt {
+public class Info {
     public static void main(String[] args) {
         System.out.println("Tobi");
-        System.out.println("T");
     }
 }
