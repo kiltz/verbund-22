1b94f08c2e2ad71ec44dfafde0c1ac294ecf7abe
Jan Kuhhirte <jeikei2006@gmail.com>
2022-01-10 15:00:22 +0100

    Aufgabe 2

--- src/main/java/basic/aufgabe1/Info.java
+++ src/main/java/basic/aufgabe1/Info.java
@@ -4,4 +4,5 @@ public class Info {
     public static void main(String[] args) {
         System.out.println("Jan Kuhhirte");
     }
+
 }
--- src/main/java/basic/aufgabe2/WuerfelStatistik.java
+++ src/main/java/basic/aufgabe2/WuerfelStatistik.java
@@ -25,9 +25,41 @@ package basic.aufgabe2;
 public class WuerfelStatistik {
 	public static void main(String[] args) {
 
-
-		double zufall = Math.random();
-
-		// hier gehts los....
+		int eins = 0;
+		int zwei = 0;
+		int drei = 0;
+		int vier = 0;
+		int fuenf = 0;
+		int sechs =0;
+		for (int i = 1; i <= 10000; ++i) {
+			double zufall = Math.random();
+			int zahl = (int) (zufall * 6) + 1;
+			// System.out.println(zahl);
+			if (zahl == 1){
+				eins++;
+			}
+			else if (zahl == 2){
+				zwei++;
+			}
+			else if (zahl == 3){
+				drei++;
+			}
+			else if (zahl == 4){
+				vier++;
+			}
+			else if (zahl == 5){
+				fuenf++;
+			}
+			else{
+				sechs++;
+			}
+		}
+		System.out.println("Anzahl der gewuerfelten Zahlen:"
+				+ "\n1: " + eins
+				+ "\n2: " + zwei
+				+ "\n3: " + drei
+				+ "\n4: " + vier
+				+ "\n5: " + fuenf
+				+ "\n6: " + sechs);
 	}
 }
