79ffc9c9df1e1d7663ceff4b59e4026821a3ca58
Nikita Mysin <nikita.mysin.fuerth@gmail.com>
2022-01-10 15:06:59 +0100

    Nikita Mysin (WuerfelStatistik2)

--- src/main/java/basic/aufgabe2/KaffeeAutomat.java
+++ src/main/java/basic/aufgabe2/KaffeeAutomat.java
@@ -3,18 +3,55 @@ package basic.aufgabe2;
 public class KaffeeAutomat {
 
 	public static void main(String[] args) {
-		// Angenommen: Ein Kaffee kostet 30 Cent,
-		// wir werfen eine 2-Euro Münze in den Automat
-		// Welche Münzen bekommen wir als Rückgeld?
+		String[] muenzen = {"1 Euro", "50 cent", "20 cent", "10 cent" , "5 cent", "2 cent", "1 cent"};
 
-		// 1. definiere den Preis und das gezahlte Geld am Anfang der Methode
-
-		// 2. Ermittle mit Hilfe von Modulo die Anzahl der einzelnen
-		// 		Münzen und arbeite mit dem Rest weiter.
-
-		// 3. Vereinfache die Lösung mit Schleifen
-
-
+		try{
+			int[] rueckgeld = ruekgeldRechnen(30, 50, "Cent");
+			for (int i = 0; i < muenzen.length; i++) {
+				if (rueckgeld[i] > 0) System.out.println(String.format("%s-Münze: %d Stück", muenzen[i], rueckgeld[i]));
+			}
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
+	static int[] ruekgeldRechnen(int preis, int summe, String muenze) throws IllegalArgumentException {
+		int[] rueckgeld = {0, 0, 0, 0, 0, 0, 0};
+		int[] muenzen = {100, 50, 20, 10, 5, 2, 1};
+		boolean check = true;
+
+		if (muenze.equalsIgnoreCase("cent")) {
+			check = false;
+			for (int m = 1; m < muenzen.length; m++) {
+				if (summe == muenzen[m]) check = true;
+			}
+		}
+
+		if (!check || summe > 2 && muenze.equalsIgnoreCase("euro"))
+			throw new IllegalArgumentException("Solche Münze existiert nicht!");
+
+		switch (muenze.toLowerCase()) {
+			case "euro":
+				summe *= 100;
+				break;
+			case "cent":
+				break;
+			default:
+				throw new IllegalArgumentException("Falshe eingabe!");
+		}
+
+		if (summe < preis || preis <= 0) throw new IllegalArgumentException("Summe ist zu klein!");
+
+		summe -= preis;
+		int temp;
+
+		for (int i = 0; i < muenze.length()-1; i++) {
+			temp = summe/muenzen[i];
+			if (temp > 0) {
+				rueckgeld[i] += temp;
+				summe -= muenzen[i] * temp;
+			}
+		}
+		return rueckgeld;
+	}
 }
