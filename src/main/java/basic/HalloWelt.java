6c9beda8c638f0e015e44deb6a2f4de5cee05037
Moritz Nowi <moritz.nowi2000@gmail.com>
2022-01-10 11:04:50 +0100

    Hallo

--- src/main/java/basic/HalloWelt.java
+++ src/main/java/basic/HalloWelt.java
@@ -2,6 +2,8 @@ package basic;
 
 public class HalloWelt {
     public static void main(String[] args) {
-        System.out.println("Hallo Welt!");
+
+        System.out.println("Moritz Nowi");
+        System.out.println("TeamBank");
     }
 }
