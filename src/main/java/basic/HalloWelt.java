1308dd883fe46324ddf9feea3119ab2af83306c6
Nikita Mysin <nikita.mysin.fuerth@gmail.com>
2022-01-10 11:04:55 +0100

    Nikita Mysin

--- src/main/java/basic/HalloWelt.java
+++ src/main/java/basic/HalloWelt.java
@@ -2,6 +2,7 @@ package basic;
 
 public class HalloWelt {
     public static void main(String[] args) {
-        System.out.println("Hallo Welt!");
+        System.out.println("Nikita Mysin");
+        System.out.println("Consorsbank");
     }
 }
