0a019da3517653de7bc0293aaf429255c27e36bd
Jan Kuhhirte <jeikei2006@gmail.com>
2022-01-10 13:13:32 +0100

    Jan Info

--- src/main/java/basic/aufgabe1/Info.java
+++ src/main/java/basic/aufgabe1/Info.java
@@ -1,9 +1,7 @@
 package basic.aufgabe1;
 
-public class Info
-{
-    public static void main(String[] args)
-    {
+public class Info {
+    public static void main(String[] args) {
         System.out.println("Jan Kuhhirte");
     }
 }
