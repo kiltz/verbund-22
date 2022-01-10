143507907237ab62cdfb1fb00e07a60486630cc2
Jan Kuhhirte <jeikei2006@gmail.com>
2022-01-10 15:20:06 +0100

    Aufgabe 2

--- src/main/java/basic/aufgabe1/Info.java
+++ src/main/java/basic/aufgabe1/Info.java
@@ -1,5 +1,16 @@
 package basic.aufgabe1;
+/*
+1. Aufgabe
+----------
 
+Erstelle die Klasse basic/aufgabe1/Info
+füge der Klasse die Main-Methode hinzu.
+
+Gebe in der Klasse Deinen Namen aus.
+
+Committe und Pushe das Ergebnis.
+
+ */
 public class Info {
     public static void main(String[] args) {
         System.out.println("Jan Kuhhirte");
