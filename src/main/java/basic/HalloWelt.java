3e65cb9adeca1a6cf1c5bbe841922c9499bba487
lanabrandscher <lana.brandscher@gmail.com>
2022-01-10 11:04:44 +0100

    lanabrandscher

--- src/main/java/basic/HalloWelt.java
+++ src/main/java/basic/HalloWelt.java
@@ -2,6 +2,8 @@ package basic;
 
 public class HalloWelt {
     public static void main(String[] args) {
-        System.out.println("Hallo Welt!");
+
+        System.out.println("lanabrandscher");
+        System.out.println("Semikron");
     }
 }
