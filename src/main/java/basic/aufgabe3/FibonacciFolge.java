096cbd8e3cb1a7c6c63677bd4ee322f4518831ad
Tobias Engelmann <engelmanntobi@web.de>
2022-01-11 10:07:04 +0100

    netter Text. Tobi

--- src/main/java/basic/aufgabe3/FibonacciFolge.java
+++ src/main/java/basic/aufgabe3/FibonacciFolge.java
@@ -2,6 +2,15 @@ package basic.aufgabe3;
 
 public class FibonacciFolge {
     public static void main(String[] args) {
-        //Gebe die Fibonacci bis 100 aus
+
+        int zahl1 = 0;
+        int zahl2 = 1;
+        int ergebnis;
+        while (zahl1 <= 100) {
+            System.out.println(zahl1);
+            ergebnis = zahl1 + zahl2;
+            zahl1 = zahl2;
+            zahl2 = ergebnis;
+        }
     }
 }
