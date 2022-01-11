9da8974858a0db84d00e8640381fe000e3f8ae86
Nikita Mysin <nikita.mysin.fuerth@gmail.com>
2022-01-11 11:23:12 +0100

    Nikita Mysin (Konto2)

--- src/main/java/oop/aufgabe4/Bank.java
+++ src/main/java/oop/aufgabe4/Bank.java
@@ -14,13 +14,12 @@ public class Bank {
         }
         k.auszahlen(300);
         if (k.getKontoStand() != -100) {
+            System.out.println(k.getKontoStand());
             System.out.println("Fehler 3");
         }
         k.auszahlen(1000);
         if (k.getKontoStand() != -100) {
             System.out.println("Fehler 4");
         }
-
-
     }
 }
