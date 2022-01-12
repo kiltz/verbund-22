3f281cc9df2308f1c18fcfd733712b1a5607e920
Moritz Nowi <moritz.nowi2000@gmail.com>
2022-01-12 11:44:48 +0100

    Aufgaben umrechnung

--- src/main/java/basic/Aufgabe/Aufgabe.java
+++
@@ -1,8 +0,0 @@
-package basic.Aufgabe;
-
-public class Aufgabe {
-    public static void main(String[] name) {
-
-        System.out.println ("Moritz");
-    }
-}
--- src/main/java/basic/aufgabe2/KaffeeAutomat.java
+++ src/main/java/basic/aufgabe2/KaffeeAutomat.java
@@ -4,17 +4,20 @@ public class KaffeeAutomat {
 
 	public static void main(String[] args) {
 		// Angenommen: Ein Kaffee kostet 30 Cent,
-		// wir werfen eine 2-Euro Münze in den Automat
-		// Welche Münzen bekommen wir als Rückgeld?
+        // wir werfen eine 2-Euro Münze in den Automat
+        // Welche Münzen bekommen wir als Rückgeld?
 
-		// 1. definiere den Preis und das gezahlte Geld am Anfang der Methode
+        // 1. definiere den Preis und das gezahlte Geld am Anfang der Methode
 
-		// 2. Ermittle mit Hilfe von Modulo die Anzahl der einzelnen
-		// 		Münzen und arbeite mit dem Rest weiter.
+        // 2. Ermittle mit Hilfe von Modulo die Anzahl der einzelnen
+        // 		Münzen und arbeite mit dem Rest weiter.
 
-		// 3. Vereinfache die Lösung mit Schleifen
+        // 3. Vereinfache die Lösung mit Schleifen
+
+        float kaffee = 0.0f;
+        float gezahlt = 2.0f;
 
 
-	}
+    }
 
 }
