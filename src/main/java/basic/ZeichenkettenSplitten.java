a3b0362d7f15520f173bf5a02987779a767f07da
Jan Kuhhirte <jeikei2006@gmail.com>
2022-01-13 14:10:55 +0100

    Aufgabe 10 ZeichenkettenSplitten

--- src/main/java/basic/ZeichenkettenSplitten.java
+++ src/main/java/basic/ZeichenkettenSplitten.java
@@ -6,7 +6,7 @@ public class ZeichenkettenSplitten {
                 "13.01.2022;Telefon;45\n" +
                 "13.01.2022;Bar;150\n" +
                 "13.01.2022;Versicherung;156";
-
+        /*
         String eineZeile = "13.01.2022;Bar;70";
 
         String[] teile = eineZeile.split(";");
@@ -15,18 +15,31 @@ public class ZeichenkettenSplitten {
             int zahl = Integer.parseInt(teile[2]);
             System.out.println(zahl);
         }
+        */
+
         // 1. Splittet den text nach dem \n => zeilen
-
+        String[] zeilen = text.split("\n");
         // (definiere eine Summe)
-
+        int summe = 0;
         // 2. für jede Zeile
-
-        // 3. Splitte nach dem ;
-
-        // 4. summiere die Beträge (Umwandeln String -> int, summe += betrag)
-
-
+        System.out.println(zeilen);
+        int zeilenAnzahl = 0;
+        String[] parts;
+        int betrag;
+        for (int i = 0; i < zeilen.length; ++i) {
+            // 3. Splitte nach dem ;
+            parts = zeilen[i].split(";");
+            // 4. summiere die Beträge (Umwandeln String -> int, summe += betrag)
+            System.out.println();
+            if (parts.length == 3) {
+                betrag = Integer.parseInt(parts[2]);
+                summe += betrag;
+            } else {
+                System.out.println("Da ist etwas schief gelaufen");
+            }
+        }
         // 5. Summe ausgeben.
+        System.out.println("Summe = " + summe);
 
         // Zusatz: Statistik für gleiche Bezeichnungen (Map?)
     }
