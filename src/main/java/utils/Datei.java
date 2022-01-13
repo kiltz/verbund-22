d56ca84fb74f2db3b87a7fda2e830bc4a0c1ad28
Jan Kuhhirte <jeikei2006@gmail.com>
2022-01-13 10:21:59 +0100

    Aufgabe 8 - Datei erstellen, schreiben unf lesen

---
+++ src/main/java/utils/Datei.java
@@ -0,0 +1,51 @@
+package utils;
+
+import java.io.*;
+
+public class Datei {
+    private String dateiName;
+    private StringBuffer inhalt;
+
+    public Datei(String dateiName) {
+        this.dateiName = dateiName;
+    }
+
+    public void schreibe(String text) {
+        schreibe(text, false); //
+    }
+
+    public void schreibe(String text, boolean append) {
+        File datei = null;
+        datei = new File(dateiName);
+        try (FileWriter outStream = new FileWriter(datei, append)) {
+
+            outStream.write(text);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String lese() {
+        StringBuffer inhalt = new StringBuffer();
+        BufferedReader reader = null;
+        File datei = null;
+        datei = new File(dateiName);
+        try (FileReader inStream = new FileReader(datei)) {
+            reader = new BufferedReader(inStream);
+            String zeile = "";
+            while ((zeile = reader.readLine()) != null) // bis alles drin ist
+            {
+                if (!inhalt.isEmpty()) {
+                    inhalt.append("\n");
+                }
+                inhalt.append(zeile);
+            }
+            // Ausgabe der gesammelten Werke
+            System.out.println(inhalt);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return inhalt.toString();
+    }
+}
