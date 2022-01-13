6daaf2aed8144ca564a5ad38d23a60392063dbf4
Moritz Nowi <moritz.nowi2000@gmail.com>
2022-01-13 10:17:46 +0100

    DateiTester Finish

--- src/main/java/features/inout/aufgabe/DateiTester.java
+++ src/main/java/features/inout/aufgabe/DateiTester.java
@@ -1,7 +1,5 @@
 package features.inout.aufgabe;
 
-import utils.Datei;
-
 /**
  * Invariante: Ändere diese Klasse nicht!
  * 
@@ -29,11 +27,9 @@ public class DateiTester {
 		System.out.println("'" + inhalt + "'");
 		System.out.println("'" + zeile1 + zeile2 + "'");
 		if (!inhalt.equals(zeile1 + zeile2)) {
-			System.out.println("Fehler: das was gelesen wurde ist nicht gleich dem was geschrieben wurde!");
+			System.err.println("Fehler: das was gelesen wurde ist nicht gleich dem was geschrieben wurde!");
 		}
 
-		// Zusatzaufgabe (für die die Klasse geändert werden darf):
-		// Erzeuge eine 4 GB große Text-Datei und lese sie ein.
 
 	}
 
