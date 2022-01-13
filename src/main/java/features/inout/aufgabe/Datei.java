cc5338c28cb227809529e41c31eed99f2636fb57
Marcel Bernast <marcel.bernast@gmail.com>
2022-01-13 10:20:04 +0100

    .

--- src/main/java/features/inout/aufgabe/Datei.java
+++ src/main/java/features/inout/aufgabe/Datei.java
@@ -1,15 +1,16 @@
 package features.inout.aufgabe;
 
 public class Datei {
-
-    public Datei(String s) {
+    public Datei(String dateiName) {
     }
 
-    public void schreibe(String zeile1, boolean b) {
+    public void schreibe(String text) {
+    }
 
+    public void schreibe(String text, boolean append) {
     }
 
     public String lese() {
-
+        return "";
     }
 }
