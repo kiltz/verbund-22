7e6c0ce4c0331bb5251e922d4e9fbc5303d24f2f
Friedrich Kiltz <f@kiltz.de>
2022-01-13 09:08:52 +0100

    1. Aufgabe IntelliJ glücklich gemacht

---
+++ src/main/java/utils/muloe/Datei.java
@@ -0,0 +1,16 @@
+package utils.muloe;
+
+public class Datei {
+    public Datei(String dateiName) {
+    }
+
+    public void schreibe(String text) {
+    }
+
+    public void schreibe(String text, boolean append) {
+    }
+
+    public String lese() {
+        return "";
+    }
+}
