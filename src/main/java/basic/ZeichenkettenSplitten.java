5dcead494be03c1c2a19d0f0ce923ede7756e717
Dmitry <dmitry.pakulov@gmail.com>
2022-01-13 13:41:36 +0100

    Aufgabe 9: Splitten Dmitry

--- src/main/java/basic/ZeichenkettenSplitten.java
+++ src/main/java/basic/ZeichenkettenSplitten.java
@@ -1,32 +1,41 @@
 package basic;
 
+import java.util.HashMap;
+
 public class ZeichenkettenSplitten {
     public static void main(String[] args) {
         String text = "13.01.2022;Bar;70\n" +
                 "13.01.2022;Telefon;45\n" +
                 "13.01.2022;Bar;150\n" +
                 "13.01.2022;Versicherung;156";
-
         String eineZeile = "13.01.2022;Bar;70";
+        int summe = 0;
+        HashMap<String, Integer> statistik = new HashMap<>();
+//        String[] teile = eineZeile.split(";");
+//        if (teile.length == 3) {
+//            System.out.println("Betrag: " + teile[2]);
+//            int zahl = Integer.parseInt(teile[2]);
+//            System.out.println(zahl);
+//        }
 
-        String[] teile = eineZeile.split(";");
-        if (teile.length == 3) {
-            System.out.println("Betrag: " + teile[2]);
-            int zahl = Integer.parseInt(teile[2]);
-            System.out.println(zahl);
+        String[] textInZeilen = text.split("\n");
+        for (String t : textInZeilen) {
+            String[] bestandteile = t.split(";");
+            String key = bestandteile[1];
+            int value = Integer.parseInt(bestandteile[2]);
+            summe += value;
+            if (!statistik.containsKey(key)) {
+                statistik.put(key, value);
+            } else {
+                value += statistik.get(key);
+                statistik.replace(key, value);
+            }
         }
-        // 1. Splittet den text nach dem \n => zeilen
+        statistik.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        });
+        System.out.println("Summe: " + summe);
 
-        // (definiere eine Summe)
-
-        // 2. für jede Zeile
-
-        // 3. Splitte nach dem ;
-
-        // 4. summiere die Beträge (Umwandeln String -> int, summe += betrag)
-
-
-        // 5. Summe ausgeben.
 
         // Zusatz: Statistik für gleiche Bezeichnungen (Map?)
     }
