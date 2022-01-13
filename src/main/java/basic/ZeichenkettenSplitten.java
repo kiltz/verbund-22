b07d6362fb5427bd1f8126a8729702e785102ff0
Marcel Bernast <marcel.bernast@gmail.com>
2022-01-13 15:58:05 +0100

    was nettes

--- src/main/java/basic/ZeichenkettenSplitten.java
+++ src/main/java/basic/ZeichenkettenSplitten.java
@@ -1,35 +1,26 @@
 package basic;
 
-import utils.Zahlen;
-
-import java.text.ParseException;
-
 public class ZeichenkettenSplitten {
     public static void main(String[] args) {
-        String text = "13.01.2022;Bar;70,00\n" +
-                "13.01.2022;Telefon;45,12\n" +
-                "13.01.2022;Bar;müll\n" +
+        String text = "13.01.2022;Bar;70\n" +
+                "13.01.2022;Telefon;45\n" +
+                "13.01.2022;Bar;150\n" +
                 "#ein Kommentar\n" +
-                "13.01.2022;Versicherung;156,85";
+                "13.01.2022;Versicherung;156";
 
         String eineZeile = "13.01.2022;Bar;70";
 
         String[] teile = eineZeile.split(";");
         if (teile.length == 3) {
             System.out.println("Betrag: " + teile[2]);
-            double zahl = 0;
-            try {
-                zahl = Zahlen.stringToDouble(teile[2]);
-                System.out.println(zahl);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+            int zahl = Integer.parseInt(teile[2]);
+            System.out.println(zahl);
         }
         // 1. Splittet den text nach dem \n => zeilen
         String[] zeilen = text.split("\n");
 
         // (definiere eine Summe)
-        double summe = 0;
+        int summe = 0;
 
         // 2. für jede Zeile
         for (int i = 0; i < zeilen.length; ++i) {
@@ -38,19 +29,14 @@ public class ZeichenkettenSplitten {
 
             // 4. summiere die Beträge (Umwandeln String -> int, summe += betrag)
             if (teile.length == 3) {
-                double zahl = 0;
-                try {
-                    zahl = Zahlen.stringToDouble(teile[2]);
-                    summe += zahl;
-                } catch (ParseException e) {
-                    System.err.println("Die Zahl " + teile[2] + " ist nicht umwandelbar: " + e.getMessage());
-                }
+                int zahl = Integer.parseInt(teile[2]);
+                summe += zahl;
             }
 
         }
 
         // 5. Summe ausgeben.
-        System.out.println("Summe: " + Zahlen.doubleToString(summe));
+        System.out.println("Summe: " + summe);
         // Zusatz: Statistik für gleiche Bezeichnungen (Map?)
     }
 }
