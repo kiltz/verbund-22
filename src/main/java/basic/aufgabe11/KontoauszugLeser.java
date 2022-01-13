1ce7aa9d50858ea8d44fe5316ab8e221e862c20e
Jan Kuhhirte <jeikei2006@gmail.com>
2022-01-13 15:25:52 +0100

    Aufgabe 11 KontoauszugLeser - mit etwas komischer Ausgabe

--- src/main/java/basic/aufgabe11/KontoauszugLeser.java
+++ src/main/java/basic/aufgabe11/KontoauszugLeser.java
@@ -1,21 +1,40 @@
 package basic.aufgabe11;
 
+import utils.Datei;
+import utils.Zahlen;
+
+import java.text.ParseException;
+
 public class KontoauszugLeser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        double summe = 0.0;
         // 1. Lese die Datei "kontoauszug.csv" ein
-
+        Datei d = new Datei("kontoauszug.csv");
+        String text = d.lese();
         // 2.Zerlege den Text in die Zeilen
-
+        String[] zeilen = text.split("\n");
         // 3. Zerlege jede Zeile
+        for (int i = 0; i < zeilen.length; ++i) {
+            // 3. Splitte nach dem ;
+            String[] parts = zeilen[i].split(";");
+            // 4. Summiere die Beträge auf
+            if (parts.length == 3) {
+                double betrag = Zahlen.stringToDouble(parts[2]);
+                summe += betrag;
+            } else {
+                System.out.println("");
+            }
+            // 5. gebe die Summe aus
+            System.out.println("-------\nSumme: " + summe + "\n");
+            // Variiere die Datei!
+            String inhalt = d.lese();
+            if (!inhalt.equals(text)) {
+                System.out.println("Fehler: das was gelesen wurde ist nicht gleich dem was geschrieben wurde!");
+            }
+            // Zusatzaufgabe:
+            // Erstellt 500 Buchungen mit unterschiedlichen Buchungstexten und Beträgen
 
-        // 4. Summiere die Beträge auf
-
-        // 5. gebe die Summe aus
-
-        // Variiere die Datei!
-
-        // Zusatzaufgabe:
-        // Erstellt 500 Buchungen mit unterschiedlichen Buchungstexten und Beträgen
-
+        }
     }
 }
+
