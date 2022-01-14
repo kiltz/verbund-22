7b911fb241fa4129b36e6b5b1bb6a7a25b6c5831
Nikita Mysin <nikita.mysin.fuerth@gmail.com>
2022-01-14 12:22:57 +0100

    Nikita Mysin (Rechner2)

--- src/main/java/gui/Rechner.java
+++ src/main/java/gui/Rechner.java
@@ -5,8 +5,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -18,20 +19,46 @@ public class Rechner extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label l = new Label("5 + 2 = ?");
-        l.setFont(new Font("Arial", 30));
-        l.setTextFill(Color.web("#0076a3"));
+        Font myFont = new Font("Arial", 20);
+
+        TextField f1 = new TextField();
+        f1.setPrefColumnCount(2);
+        f1.setFont(myFont);
+
+        Label l1 = new Label("+");
+        l1.setFont(myFont);
+
+        TextField f2 = new TextField();
+        f2.setPrefColumnCount(2);
+        f2.setFont(myFont);
+
+        Label l2 = new Label("= ?");
+        l2.setFont(myFont);
+
+        HBox hBox1 = new HBox();
+        hBox1.getChildren().setAll(f1, l1, f2, l2);
+        hBox1.setAlignment(Pos.CENTER);
+        hBox1.setSpacing(5);
+
 
         Button btn = new Button();
         btn.setText("Rechnen");
-        btn.setFont(new Font("Arial", 30));
+        btn.setFont(myFont);
         btn.setOnAction(event -> {
-            l.setText("5 + 2 = 7");
-            l.setTextFill(Color.web("#FF0000"));
+            try {
+                int a = Integer.parseInt(f1.getText());
+                int b = Integer.parseInt(f2.getText());
+
+                l2.setText(String.format("= %d", a + b));
+            } catch (NumberFormatException ex) {
+                f1.setText("");
+                f2.setText("");
+                l2.setText("= ?");
+            }
         });
 
         VBox root = new VBox();
-        root.getChildren().addAll(l, btn);
+        root.getChildren().addAll(hBox1, btn);
         root.setAlignment(Pos.CENTER);
         root.setSpacing(20);
 
