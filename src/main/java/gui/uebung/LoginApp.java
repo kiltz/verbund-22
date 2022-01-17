fbd3219280039ed36578a8623dcc3830cee7d7c8
Dmitry <dmitry.pakulov@gmail.com>
2022-01-17 12:58:04 +0100

    LoginApp Dmitry

--- src/main/java/gui/uebung/LoginApp.java
+++ src/main/java/gui/uebung/LoginApp.java
@@ -1,9 +1,14 @@
 package gui.uebung;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -25,17 +30,56 @@ import javafx.stage.Stage;
  */
 public class LoginApp extends Application {
 
+    Label lBN;
+    Label lPW;
+    TextField benutzername;
+    TextField passwort;
+    Button login;
+
     public static void main(String[] args) {
         launch(null);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Label l = new Label("Hallo App!");
-        HBox root = new HBox();
-        root.getChildren().add(l);
-        Scene scene = new Scene(root, 300, 400);
+        lBN = new Label("Benutzername:");
+        lPW = new Label("Passwort:");
+        benutzername = new TextField();
+        passwort = new TextField();
+        login = new Button("Login");
+        login.setOnAction(e -> setLogin(e));
+
+        VBox root = new VBox();
+        HBox inputBox = new HBox();
+        VBox labels = new VBox();
+        VBox textFields = new VBox();
+
+        benutzername.setMaxWidth(250);
+        passwort.setMaxWidth(250);
+        labels.getChildren().addAll(lBN, lPW);
+        labels.setSpacing(6);
+        textFields.getChildren().addAll(benutzername, passwort);
+        inputBox.getChildren().addAll(labels, textFields);
+        inputBox.setSpacing(10);
+        Insets insets = new Insets(10, 10, 0, 10);
+        root.setPadding(insets);
+        root.getChildren().add(inputBox);
+        root.getChildren().add(login);
+        Scene scene = new Scene(root, 300, 100);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    private void setLogin(ActionEvent e) {
+        if (benutzername.getText().matches("benutzername") && passwort.getText().matches("passwort")) {
+            System.out.println("Anmeldung erfolgt...");
+        } else if (benutzername.getText().isEmpty()) {
+            benutzername.requestFocus();
+            benutzername.setPromptText("Bitte Benutzernamen eingeben...");
+        } else if (passwort.getText().isEmpty()) {
+            passwort.requestFocus();
+            passwort.setPromptText("Bitte Passwort eingeben...");
+        }
+    }
+
 }
