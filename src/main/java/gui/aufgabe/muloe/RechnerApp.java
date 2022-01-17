df4ec896bd3cab19e778a05bb12c690bbaf5f149
Friedrich Kiltz <f@kiltz.de>
2022-01-17 09:51:11 +0100

    RechnerApp mit Action

--- src/main/java/gui/aufgabe/muloe/RechnerApp.java
+++ src/main/java/gui/aufgabe/muloe/RechnerApp.java
@@ -1,6 +1,7 @@
 package gui.aufgabe.muloe;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -8,6 +9,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import utils.Zahlen;
+
+import java.text.ParseException;
 
 /*
 Aufgabe:
@@ -22,19 +26,25 @@ Keine Funktionalität!
 public class RechnerApp extends Application {
 
 
+    private TextField tfEins;
+    private TextField tfZwei;
+    private Label lErgebnis;
+
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Label lErgebnis = new Label("7");
+        lErgebnis = new Label("7");
         Label lPlus = new Label(" + ");
-        TextField tfEins = new TextField("5");
+        tfEins = new TextField("5");
         tfEins.setMaxWidth(50);
-        TextField tfZwei = new TextField("2");
+        tfZwei = new TextField("2");
         tfZwei.setMaxWidth(50);
         Button bRechne = new Button(" = ");
+        bRechne.setDefaultButton(true);
+        bRechne.setOnAction(e -> rechne(e));
 
         HBox root = new HBox();
         root.setSpacing(10);
@@ -45,6 +55,30 @@ public class RechnerApp extends Application {
         primaryStage.setTitle("Rechner");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void rechne(ActionEvent e) {
+        // 1. Textfelder auslesen
+        String textEins = tfEins.getText();
+        String textZwei = tfZwei.getText();
+        try {
+            // 2. String in Double umwandeln
+            double zahlEins = Zahlen.stringToDouble(textEins);
+            double zahlZwei = Zahlen.stringToDouble(textZwei);
+            // 3. Rechnen
+            double ergebnis = zahlEins + zahlZwei;
+            // 4. Ergebnis ausgeben
+            lErgebnis.setText(Zahlen.doubleToString(ergebnis));
+            // 5. Inhalt der Textfelder löschen (opt.)
+
+            // 6. Fokus in das erste Textfeld setzen
+            tfEins.requestFocus();
+
+        } catch (ParseException ex) {
+            // Mecker!
+            System.out.println(ex.getMessage());
+        }
+
     }
 
 }
