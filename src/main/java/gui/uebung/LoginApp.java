d3686a8d3bf62f7a67ae930f5b3d9c1b2bc741da
Jan Kuhhirte <jeikei2006@gmail.com>
2022-01-17 16:01:28 +0100

    Aufgabe 13 LoginApp

--- src/main/java/gui/uebung/LoginApp.java
+++ src/main/java/gui/uebung/LoginApp.java
@@ -1,12 +1,12 @@
 package gui.uebung;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -32,37 +32,46 @@ import javafx.stage.Stage;
  */
 public class LoginApp extends Application {
 
+    private TextField tfUserName;
+    private TextField tfPassword;
+
+
     public static void main(String[] args) {
-        launch(null);
+        launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         VBox root = new VBox();
-        root.setPadding(new Insets(20, 20, 20, 20));
+        root.setPadding(new Insets(20));
         root.setSpacing(15);
         primaryStage.setTitle("Login");
-        root.getChildren().addAll(getUsernameAndPassword(), loginButton());
-        getUsernameAndPassword().setDisable(true);
-        Scene scene = new Scene(root, 500, 600);
+        root.getChildren().addAll(getUsernameAndPassword(), loginButton(), pruefeEingabe());
+        Scene scene = new Scene(root, 300, 100);
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
-    private Node getUsernameAndPassword() {
-        HBox box = new HBox();
-        box.setSpacing(15);
-        TextField tfUserName = new TextField();
-        tfUserName.setPromptText("Enter Username");
-        tfUserName.setMaxWidth(120);
-        tfUserName.setFocusTraversable(false);
-        TextField tfPassword = new TextField();
-        tfPassword.setPromptText("Enter Password");
-        tfPassword.setMaxWidth(120);
-        tfPassword.setFocusTraversable(false);
-        tfPassword.setOnAction(e -> System.out.println("Eingabe: " + tfUserName.getText() + " " + tfPassword.getText()));
-        box.getChildren().addAll(tfUserName, tfPassword);
-        return box;
+    private Node pruefeEingabe() {
+        VBox vBox = new VBox();
+//        if (loginButton().isPressed()) { // Funktioniert vielleicht nicht?
+        if (tfUserName.getText().trim().isEmpty()) {
+            Label unErr = new Label();
+            vBox.getChildren().add(unErr);
+        } else {
+            vBox = new VBox();
+        }
+        if (tfPassword.getText().trim().isEmpty()) {
+            Label pwErr = new Label();
+            vBox.getChildren().add(pwErr);
 
+        } else {
+            vBox = new VBox();
+        }
+        tfUserName.setPromptText("Enter Username");
+        tfPassword.setPromptText("Enter Password");
+        return vBox;
     }
 
     private Node loginButton() {
@@ -71,24 +80,24 @@ public class LoginApp extends Application {
         Button bLogin = new Button("Login");
         box.getChildren().addAll(bLogin);
         bLogin.setDefaultButton(true);
-        bLogin.setOnAction(this::loginButtonAction);
+        bLogin.setStyle("-fx-background-color: #dfdfdf; ");
+        bLogin.setOnAction(event -> pruefeEingabe());
         return box;
-    }
-
-    public void loginButtonAction(ActionEvent event) {
-        // Validiert den Dialog
-        // * 1. Benutzername darf nicht leer sein
-        // * 2. Passwort darf nicht leer sein
-        // * 3. Benutzernamen und Passwort müssen irgendwelchen festen Werten entsprechen
 
     }
 
-    private Node loggedIn() {
-        HBox box = new HBox();
-        Label lLoggedIn = new Label("Logged in");
-        // wenn Validation ok --> "Logged in"
-        box.getChildren().addAll(lLoggedIn);
-        return box;
+    private Node getUsernameAndPassword() {
+        HBox hBox = new HBox();
+        hBox.setSpacing(15);
+        tfUserName = new TextField();
+        tfUserName.setMaxWidth(120);
+        tfUserName.setFocusTraversable(false);
+        tfPassword = new PasswordField();
+        tfPassword.setMaxWidth(120);
+        tfPassword.setFocusTraversable(false);
+        hBox.getChildren().add(tfUserName);
+        hBox.getChildren().add(tfPassword);
+        return hBox;
     }
 }
 
