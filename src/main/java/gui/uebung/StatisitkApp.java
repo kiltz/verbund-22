8e9d436685014d8718092634e0ed345c820f3bf8
Friedrich Kiltz <f@kiltz.de>
2022-01-18 09:10:07 +0100

    Anmerlungen

--- src/main/java/gui/uebung/StatisitkApp.java
+++ src/main/java/gui/uebung/StatisitkApp.java
@@ -1,19 +1,16 @@
 package gui.uebung;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import utils.Zahlen;
-
-import java.text.ParseException;
 
 /**
  * Aufgabe:
@@ -38,34 +35,45 @@ public class StatisitkApp extends Application {
         VBox root = new VBox();
         root.setPadding(new Insets(10, 20, 20, 20));
         root.setSpacing(15);
+        // TODO: Buttons und Textfelder dem root hinzufügen
 
-        Scene scene = new Scene();
+        // TODO: lErgebnis erzeugen und root hinzufügen
+
+        // TODO Breite und Höhe setzen
+        Scene scene = new Scene(root);
         primaryStage.setTitle("Rechner");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        private Node getTextFelder () {
-            HBox box = new HBox();
-            box.setSpacing(15);
-            TextField tf = new TextField();
-            tf.setPromptText("Ganzzahlige Werte eingeben!");
-            tf.setOnAction(e ->
-                    System.out.println("Eingabe: " + tf.getText())
-            );
-        }
-        ;
-
-        private Node getButtons () {
-            HBox box = new HBox();
-            box.setSpacing(15);
-            Button bRechne = new Button(" = ");
-            bRechne.setDefaultButton(true);
-            bRechne.setOnAction(e -> rechne(e));
-        }
-
-        private void rechne (ActionEvent e){
-            String tf.getText();
-
-        }
     }
+
+    private Node getButtons() {
+        HBox box = new HBox();
+        box.setSpacing(15);
+        Button bRechne = new Button(" = ");
+        bRechne.setDefaultButton(true);
+        bRechne.setOnAction(e -> rechne(e));
+        return box;
     }
+
+    private void rechne(ActionEvent e) {
+        String eingabe = tf.getText();
+        System.out.println(eingabe);
+        // TODO: eingabe in Zahl umwandeln Integer.parseInt(...)
+
+        // TODO: Zahl mal 2 nehmen
+
+        // TODO: Ergebnis im Label lErgebnis ausgeben (int in String umwandeln ""+zahl)
+
+    }
+
+    private Node getTextFelder() {
+        HBox box = new HBox();
+        box.setSpacing(15);
+        tf = new TextField();
+        tf.setPromptText("Ganzzahlige Werte eingeben!");
+        tf.setOnAction(e ->
+                System.out.println("Eingabe: " + tf.getText())
+        );
+        return box;
+    }
+}
