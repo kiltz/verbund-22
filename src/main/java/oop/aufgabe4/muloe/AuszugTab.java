71d46ea0b527130877c97fa5c9a3bca754a4e0ff
Moritz Nowi <moritz.nowi2000@gmail.com>
2022-01-19 09:43:44 +0100

    Button Historie und zurück

--- src/main/java/oop/aufgabe4/muloe/AuszugTab.java
+++ src/main/java/oop/aufgabe4/muloe/AuszugTab.java
@@ -3,10 +3,14 @@ package oop.aufgabe4.muloe;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class AuszugTab extends BasisTab {
     private final Konto konto;
+    private Label anzeige;
+    private Button button;
+    private Button zurueck;
+
 
     public AuszugTab(Konto konto) {
         this.konto = konto;
@@ -14,14 +18,26 @@ public class AuszugTab extends BasisTab {
 
     @Override
     public Tab getTab() {
-        HBox box = new HBox();
-        Label anzeige = new Label("Dialog für Auzüge");
-        Button bEinzahlen = new Button("aktualisieren");
-        bEinzahlen.setOnAction(e ->
+        VBox box = new VBox();
+        anzeige = new Label("Dialog für Auzüge");
+        button = new Button("Historie");
+        zurueck = new Button("Zurück");
+        button.setOnAction(e ->
+                anzeige.setText(String.format("Historie", konto.getKontoStand(),
+                        box.getChildren().add(zurueck))
+                ));
+
+
+        box.setSpacing(10);
+
+        box.getChildren().add(anzeige);
+        box.getChildren().add(button);
+        //box.getChildren().add(zurueck);
+
+        Tab tab = new Tab("Auszüge", box);
+        tab.setOnSelectionChanged(e ->
                 anzeige.setText(String.format("Kontostand: %.2f", konto.getKontoStand()))
         );
-        box.getChildren().addAll(anzeige, bEinzahlen);
-        Tab tab = new Tab("Auszüge", box);
         return tab;
     }
 }
--- src/main/java/oop/aufgabe4/muloe/BuchungTab.java
+++ src/main/java/oop/aufgabe4/muloe/BuchungTab.java
@@ -24,19 +24,20 @@ public class BuchungTab extends BasisTab {
         Button bEinzahlen = new Button("Einzahlen");
         Button bAbheben = new Button("Abheben");
         bEinzahlen.setDefaultButton(true);
+
         tfEingabe = new TextField();
 
         bEinzahlen.setOnAction(e -> rechne(e));
-        bAbheben.setOnAction(e -> {
-            try {
-                konto.auszahlen(50);
-            } catch (KeineKontoDeckungException ex) {
-                ex.printStackTrace();
-            }
+        bAbheben.setOnAction(e -> abheben(e));
+        {
+
             anzeige.setText(String.format("Kontostand: %.2f", konto.getKontoStand()));
-        });
+        }
+        ;
+
+
         box.setSpacing(10);
-        //box.getChildren().add(tfEingabe);
+
         box.getChildren().add(tfEingabe);
         box.getChildren().add(anzeige);
         box.getChildren().add(bEinzahlen);
@@ -55,8 +56,28 @@ public class BuchungTab extends BasisTab {
         }
     }
 
+    private void abheben(ActionEvent e) {
+        String eingabe = tfEingabe.getText();
+        if (eingabe.isEmpty()) {
+            tfEingabe.setPromptText("Bitte Zahlen Eingeben!");
+        } else {
+            berechneAbhebung(eingabe);
+
+        }
+    }
+
     private void berechneEingabe(String eingabe) {
         konto.einzahlen(Double.parseDouble(eingabe));
+        anzeige.setText(String.format("Kontostand: %.2f", konto.getKontoStand()));
+
+    }
+
+    private void berechneAbhebung(String eingabe) {
+        try {
+            konto.auszahlen(Double.parseDouble(eingabe));
+        } catch (KeineKontoDeckungException e) {
+            e.printStackTrace();
+        }
         anzeige.setText(String.format("Kontostand: %.2f", konto.getKontoStand()));
 
     }
