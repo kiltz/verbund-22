ed30871efa9fd2757a56470a52952151638487af
Jan Kuhhirte <jeikei2006@gmail.com>
2022-01-19 15:37:20 +0100

    Aufgabe 16 EditorApp - button Funktionieren, speichert/liest NICHT! #2

--- src/main/java/gui/uebung/aufgabe16/EditorApp.java
+++ src/main/java/gui/uebung/aufgabe16/EditorApp.java
@@ -5,10 +5,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -19,7 +16,10 @@ import java.io.FileWriter;
 
 public class EditorApp extends Application {
     private TextArea taTextEditor;
-    private String datei;
+    private String dateiName;
+    private String dateiPfad;
+    private TextField dateiPfadName;
+    private Button bSave;
 
     public static void main(String[] args) {
         launch(args);
@@ -30,7 +30,7 @@ public class EditorApp extends Application {
         BorderPane root = new BorderPane();
         root.setPadding(new Insets(10));
         root.setTop(dateiEinlesen());
-        root.setCenter(textEditor());
+        root.setCenter(getTextFromArea());
         root.setBottom(speichernBeenden());
         Scene scene = new Scene(root, 500, 500);
         primaryStage.setTitle("Editor");
@@ -45,34 +45,41 @@ public class EditorApp extends Application {
         node.setVgap(20);
         node.setHgap(20);
         Label dateiLabel = new Label("Datei");
-        TextField dateiPfad = new TextField();
-        dateiPfad.setPromptText("Dateipfad:");
+        dateiPfadName = new TextField();
+        dateiPfadName.setPromptText("Dateipfad:");
         Button bLesen = new Button("lesen");
-        datei = dateiPfad.getText();
-        dateiPfad.setFocusTraversable(false);
+        dateiName = dateiPfadName.getText();
+        dateiPfadName.setFocusTraversable(false);
         bLesen.setFocusTraversable(true);
         node.getChildren().add(dateiLabel);
-        node.getChildren().add(dateiPfad);
+        node.getChildren().add(dateiPfadName);
         node.getChildren().add(bLesen);
         bLesen.setOnAction(event -> Datei.lese());
         new Button("Lesen");
         return node;
     }
 
-    private Node textEditor() {
+    private Node getTextFromArea() {
         FlowPane node = new FlowPane();
         node.setAlignment(Pos.CENTER);
         taTextEditor = new TextArea("");
-        node.getChildren().add(taTextEditor);
-        return node;
+        ScrollPane scrolli = new ScrollPane();
+        scrolli.setContent(taTextEditor);
+        scrolli.setPadding(new Insets(20));
+        scrolli.setFitToHeight(true);
+        scrolli.setFitToWidth(true);
+        return scrolli;
     }
 
     private Node speichernBeenden() {
         FlowPane node = new FlowPane();
-        Button bSave = new Button("Speichern");
+        bSave = new Button("Speichern");
         Button bEnd = new Button("Beenden");
         bSave.setOnAction(event -> speichern());
         bEnd.setOnAction(event -> System.exit(0));
+        // wenn nach speichern, text in file == text in scrolli -->
+        // Button text = "Gespeichert"
+        // wenn text geändert wird --> Button text = "Speichern"
         node.getChildren().add(bSave);
         node.getChildren().add(bEnd);
         node.setVgap(10);
@@ -82,9 +89,16 @@ public class EditorApp extends Application {
     }
 
     private void speichern() {
-        File f = new File(String.format("%s"), datei);
-        String absolute = f.getAbsolutePath();
-        try (FileWriter fw = new FileWriter(absolute, false)) {
+        File f = new File(String.format("\"C:\\%s.txt"), dateiName);
+        dateiPfad = f.getAbsolutePath();
+        try {
+            if (dateiPfad.length() >= 0) {
+                dateiPfadName.setText(dateiPfad);
+            }
+        } catch (Exception e) {
+            System.out.println("Noch nichts eingegeben");
+        }
+        try (FileWriter fw = new FileWriter(dateiPfad, false)) {
             fw.write(taTextEditor.getText());
         } catch (Exception e) {
             System.err.println(e.getMessage());
