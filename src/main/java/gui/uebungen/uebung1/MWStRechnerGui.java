a08c88740935b84757290fe227af8ed1e5b99b6c
lanabrandscher <lana.brandscher@gmail.com>
2022-01-20 09:42:18 +0100

    Test

---
+++ src/main/java/gui/uebungen/uebung1/MWStRechnerGui.java
@@ -0,0 +1,57 @@
+package gui.uebungen.uebung1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import utils.Zahlen;
+
+import java.text.ParseException;
+
+public class MWStRechnerGui extends Application {
+
+    public TextField tf_betragseingabe = new TextField();
+    public Label l_ergebnis = new Label();
+
+    public static void main(String[] args) {
+        launch(null);
+    }
+
+    public void rechne() throws ParseException {
+        String s_betragseingabe = tf_betragseingabe.getText();
+        double d_betragseingabe = Zahlen.stringToDouble(s_betragseingabe);
+
+        double d_ergebnis = (d_betragseingabe / 100 * 119);
+        String s_ergebnis = Zahlen.doubleToString(d_ergebnis);
+        l_ergebnis.setText(s_ergebnis);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        tf_betragseingabe.setPromptText("Betrag (mit Komma!)");
+        Label l_mehrwertsteuer = new Label("19%");
+        Button b_rechne = new Button("=");
+
+        b_rechne.setOnAction(event -> {
+            try {
+                rechne();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            b_rechne.setDefaultButton(true);
+        });
+
+        HBox WurzelKnoten = new HBox();
+        Scene scene = new Scene(WurzelKnoten, 700, 200);
+        WurzelKnoten.getChildren().addAll(tf_betragseingabe, l_mehrwertsteuer, b_rechne, l_ergebnis);
+        WurzelKnoten.setSpacing(10);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Mehrwertsteuerrechner");
+        primaryStage.show();
+
+    }
+}
