30b830b311bfe308dad72d138a99c09449ba2c48
Jan Kuhhirte <jeikei2006@gmail.com>
2022-01-20 15:08:17 +0100

    pwmanager toggle PW visibility

--- src/main/java/de/verbund/pwmanager/gui/PWManagerController.java
+++ src/main/java/de/verbund/pwmanager/gui/PWManagerController.java
@@ -1,14 +1,22 @@
 package de.verbund.pwmanager.gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 
 
 public class PWManagerController {
 
+    private String strSearch;
     @FXML
     public TextField tfSearchbar;
-    private String strSearch;
+    @FXML
+    private ToggleButton bPWToggleVis;
+    @FXML
+    private TextField tfNewPassword;
+    @FXML
+    private Label textPassword;
 
     public void doSave() {
         System.out.println("It should save with this");
@@ -25,6 +33,19 @@ public class PWManagerController {
             System.out.println("Searching: " + strSearch + "...");
         } else {
             System.out.println("Searching: nothing...");
+        }
+    }
+
+    public void doTogglePWVis() {
+        boolean isSelected = bPWToggleVis.isSelected();
+        if (isSelected) {
+            System.out.println("Toggle On");
+            textPassword.setText(tfNewPassword.getText());
+            tfNewPassword.setVisible(false);
+        } else {
+            System.out.println("Toggle Off");
+            textPassword.setText(null);
+            tfNewPassword.setVisible(true);
         }
     }
 }
